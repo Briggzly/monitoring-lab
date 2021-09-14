731ac35d38e6d39b1d3fa709cde52a006b51a160
Kyson Briggs <kysonjbriggs@gmail.com>
2021-09-14 09:59:09 -0600

    added post endpoint

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
-const express = require('express')
-const path = require('path')
-const Rollbar = require('rollbar')
+const express = require("express");
+const path = require("path");
+const Rollbar = require("rollbar");
 
 let rollbar = new Rollbar({
-    accessToken: '4eeff726083f4cecbcfca0718dffaf91',
-    captureUncaught: true,
-    captureUnhandledRejections: true
-})
+  accessToken: "4eeff726083f4cecbcfca0718dffaf91",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
-const app = express()
+const students = [];
+const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-    rollbar.info('HTML file served successfully!')
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+  rollbar.info("HTML file served successfully!");
+});
+
+app.post("/api/student", (req, res) => {
+  const { name } = req.body;
+  name = name.trim();
+  students.push(name);
+
+  rollbar.log("Student added successfully!", {
+    author: "Kyson",
+    type: "Manual Entry",
+  });
+  res.status(200).send(students);
+});
 
 const port = process.env.PORT || 4545;
 
+app.use(rollbar.errorHandler());
+
 app.listen(port, () => {
-    console.log(`server running on server ${port}`)
-})
+  console.log(`server running on server ${port}`);
+});
